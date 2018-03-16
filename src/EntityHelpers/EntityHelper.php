07643c7a6765d5589ee53d9ec0f4c67dfcb17286
Maarten Heip <maarten.heip@intracto.com>
2018-03-16 11:03:28 +0100

    MHEIP-DRUPAL: Add initial helper files to git repository

---
+++ src/EntityHelpers/EntityHelper.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: maarten.heip
+ * Date: 16/03/2018
+ * Time: 10:09
+ */
+
+namespace Mheip\Drupal\EntityHelpers;
+
+use Drupal\Core\Entity\EntityInterface;
+
+abstract class EntityHelper {
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   * @param $fieldName
+   */
+  public static function getEntityFieldValues(EntityInterface $entity, $fieldName) {
+
+  }
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   * @param $fieldName
+   *
+   * @return bool
+   */
+  public static function getEntityFieldValue(EntityInterface $entity, $fieldName) {
+    if (!$fieldValue = $entity->get($fieldName)) {
+      return FALSE;
+    }
+
+    if ($fieldValue->isEmpty()) {
+      return FALSE;
+    }
+
+    if (!$value = $fieldValue->first()->value) {
+      return FALSE;
+    }
+
+    return $value;
+  }
+
+}
---
+++ src/EntityHelpers/ParagraphHelper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: maarten.heip
+ * Date: 16/03/2018
+ * Time: 10:28
+ */
+
+namespace Mheip\Drupal\EntityHelpers;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\node\NodeInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+
+class ParagraphHelper extends EntityHelper {
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface $paragraph
+   *
+   * @return bool
+   */
+  public static function getNodeParent(EntityInterface $paragraph) {
+    $parent = $paragraph->getParentEntity();
+
+    if ($parent instanceof NodeInterface) {
+      return $parent;
+    }
+
+    if ($parent instanceof EntityInterface) {
+      return self::getNodeParent($parent);
+    }
+
+    return FALSE;
+  }
+
+}
